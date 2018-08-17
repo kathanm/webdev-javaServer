10017746c471ac9e9ceded7d9be5d23c71cba6b2
Kathan Master <kathan.master@yahoo.com>
2018-08-16 23:58:56 -0400

    Added renderUsers function to dynamically render users table

--- src/main/webapp/user-admin.controller.client.js
+++ src/main/webapp/user-admin.controller.client.js
@@ -15,5 +15,43 @@
 
     function renderUsers(users) {
         console.log(users);
+
+        var tbody = $('tbody');
+        tbody.empty();
+        for(var i=0; i<users.length; i++) {
+            var user = users[i];
+
+            var tr = $('<tr>');
+            var td = $('<td>');
+            td.append(user.username);
+            tr.append(td);
+
+            td = $('<td>');
+            td.append('********');
+            tr.append(td);
+
+            td = $('<td>');
+            td.append(user.firstName);
+            tr.append(td);
+
+            td = $('<td>');
+            td.append(user.lastName);
+            tr.append(td);
+
+            td = $('<td>');
+            td.append('hello@world.com');
+            tr.append(td);
+
+            td = $('<td>');
+            td.append('Student');
+            tr.append(td);
+
+            var deleteBtn = $('<button>DELETE</button>');
+            td = $('<td>');
+            td.append(deleteBtn);
+            tr.append(td);
+
+            tr.appendTo(tbody);
+        }
     }
 })();

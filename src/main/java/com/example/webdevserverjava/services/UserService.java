cd601c2928e497854d0a32e37854923b9cbee328
Kathan Master <kathan.master@yahoo.com>
2018-08-17 00:29:31 -0400

    Added api for deleting user by id

--- src/main/java/com/example/webdevserverjava/services/UserService.java
+++ src/main/java/com/example/webdevserverjava/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,6 +63,11 @@ public class UserService {
 	public Optional<User> findUserByUserId(@PathVariable("userId") String userId) {
 		int id = Integer.parseInt(userId);
 		return userRepository.findById(id);
+	}
+	
+	@DeleteMapping("/api/user/{userId}")
+	public void deleteUser(@PathVariable("userId") int id) {
+		userRepository.deleteById(id);
 	}
 	
 	@PostMapping("/api/logout")
